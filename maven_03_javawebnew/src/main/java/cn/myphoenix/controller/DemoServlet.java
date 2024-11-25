6ed227181c39885a34901f26ce59702707b97433
Legende_L <55222653+Acura-bit@users.noreply.github.com>
2024-11-25 21:58:34 +0800

    feat: 在learning_maven项目中导入Maven的项目

---
+++ maven_03_javawebnew/src/main/java/cn/myphoenix/controller/DemoServlet.java
@@ -0,0 +1,21 @@
+package cn.myphoenix.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo")
+public class DemoServlet extends HttpServlet {
+
+    /**
+     * 客户端 :  <a href="/demo">访问服器</a>
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("服务器被访问到了..............new");
+        req.getRequestDispatcher("/main.jsp").forward(req,resp);
+    }
+}
