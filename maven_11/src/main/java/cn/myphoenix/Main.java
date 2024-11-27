4a4234ce0fbb4ea0b8688a833c09e73f0b14caa9
Legende_L <55222653+Acura-bit@users.noreply.github.com>
2024-11-27 15:55:59 +0800

    feat: 在pom文件中指定配置文件路径防止编译丢失

---
+++ maven_11/src/main/java/cn/myphoenix/Main.java
@@ -0,0 +1,12 @@
+package cn.myphoenix;
+
+/**
+ * @author: larry_lwh
+ * @date: 2024-11-27 15:19
+ * @description:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
